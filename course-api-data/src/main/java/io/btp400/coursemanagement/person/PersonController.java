ff8f296dc1ea96656e2961ac9d95e4d5503c5a2d
ILLIAKH <39228121+ILLIAKH@users.noreply.github.com>
2020-03-31 16:06:11 -0400

    implemented authentication for admin and denied all access if correct credentials are not given

--- course-api-data/src/main/java/io/btp400/coursemanagement/person/PersonController.java
+++ course-api-data/src/main/java/io/btp400/coursemanagement/person/PersonController.java
@@ -4,6 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +16,52 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.btp400.coursemanagement.course.Course;
+import io.btp400.models.AuthenticationRequest;
+import io.btp400.models.AuthenticationResponse;
+import io.btp400.services.MyUserDetailsService;
+import io.btp400.util.JwtUtil;
 
 @RestController
 public class PersonController {
 	
 	@Autowired
 	private PersonService personService;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtUtil jwtTokenUtil;
+
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+
+	@RequestMapping({ "/hello" })
+	public String firstPage() {
+		return "<h1>Hello World</h1>";
+	}
+
+	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+
+		try {
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+			);
+		}
+		catch (BadCredentialsException e) {
+			throw new Exception("Incorrect username or password", e);
+		}
+
+
+		final UserDetails userDetails = userDetailsService
+				.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
+	}
+
 	
 	//GET ALL
 	@RequestMapping("/users")
---
+++ course-api-data/src/main/java/io/btp400/services/MyUserDetailsService.java
@@ -0,0 +1,23 @@
+package io.btp400.services;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import io.btp400.coursemanagement.person.Person;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService{
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		//Person person;
+		//new User(person.getName(),person.getPassword(),new ArrayList<>()
+		return new User("admin","123",new ArrayList<>());
+	}
+
+}
